86ec926076c8647e4606d45cbc19ac3ee2bd4be6
MM20 <15646950+MM2-0@users.noreply.github.com>
2024-06-30 19:39:23 +0200

    Version 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "de.mm20.launcher2.plugin.openweathermap"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
